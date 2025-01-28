67590bf99095e8f4bd46c9ef5bc44e0d3911ec3d
xdpirate <1757462+xdpirate@users.noreply.github.com>
2025-01-29 00:43:23 +0100

    Initial commit

---
+++ Barricadr/frmMain.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.dirPicker = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnBlock = New System.Windows.Forms.Button()
+        Me.btnAllow = New System.Windows.Forms.Button()
+        Me.btnRestore = New System.Windows.Forms.Button()
+        Me.lblPathText = New System.Windows.Forms.Label()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.lblExplanation = New System.Windows.Forms.Label()
+        Me.lblCaution = New System.Windows.Forms.Label()
+        Me.lblCautionText = New System.Windows.Forms.Label()
+        Me.chkRecursiveScan = New System.Windows.Forms.CheckBox()
+        Me.chkDarkMode = New System.Windows.Forms.CheckBox()
+        Me.lnkGithub = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(12, 304)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(258, 20)
+        Me.txtPath.TabIndex = 3
+        '
+        'dirPicker
+        '
+        Me.dirPicker.Description = "Select root directory to scan:"
+        Me.dirPicker.ShowNewFolderButton = False
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBrowse.Location = New System.Drawing.Point(276, 302)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(123, 23)
+        Me.btnBrowse.TabIndex = 4
+        Me.btnBrowse.Text = "Br&owse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnBlock
+        '
+        Me.btnBlock.BackColor = System.Drawing.Color.Pink
+        Me.btnBlock.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBlock.Location = New System.Drawing.Point(12, 330)
+        Me.btnBlock.Name = "btnBlock"
+        Me.btnBlock.Size = New System.Drawing.Size(126, 68)
+        Me.btnBlock.TabIndex = 5
+        Me.btnBlock.Tag = "block"
+        Me.btnBlock.Text = "&Block"
+        Me.btnBlock.UseVisualStyleBackColor = False
+        '
+        'btnAllow
+        '
+        Me.btnAllow.BackColor = System.Drawing.Color.PaleGreen
+        Me.btnAllow.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAllow.Location = New System.Drawing.Point(144, 330)
+        Me.btnAllow.Name = "btnAllow"
+        Me.btnAllow.Size = New System.Drawing.Size(126, 68)
+        Me.btnAllow.TabIndex = 6
+        Me.btnAllow.Tag = "allow"
+        Me.btnAllow.Text = "&Allow"
+        Me.btnAllow.UseVisualStyleBackColor = False
+        '
+        'btnRestore
+        '
+        Me.btnRestore.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btnRestore.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRestore.Location = New System.Drawing.Point(276, 331)
+        Me.btnRestore.Name = "btnRestore"
+        Me.btnRestore.Size = New System.Drawing.Size(126, 68)
+        Me.btnRestore.TabIndex = 7
+        Me.btnRestore.Tag = "restore"
+        Me.btnRestore.Text = "&Restore"
+        Me.btnRestore.UseVisualStyleBackColor = False
+        '
+        'lblPathText
+        '
+        Me.lblPathText.AutoSize = True
+        Me.lblPathText.Location = New System.Drawing.Point(12, 288)
+        Me.lblPathText.Name = "lblPathText"
+        Me.lblPathText.Size = New System.Drawing.Size(29, 13)
+        Me.lblPathText.TabIndex = 2
+        Me.lblPathText.Text = "Path"
+        '
+        'lblHeader
+        '
+        Me.lblHeader.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblHeader.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.Location = New System.Drawing.Point(12, 9)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(387, 50)
+        Me.lblHeader.TabIndex = 0
+        Me.lblHeader.Text = "Barricadr"
+        Me.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblExplanation
+        '
+        Me.lblExplanation.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblExplanation.Location = New System.Drawing.Point(12, 70)
+        Me.lblExplanation.Name = "lblExplanation"
+        Me.lblExplanation.Size = New System.Drawing.Size(387, 104)
+        Me.lblExplanation.TabIndex = 1
+        Me.lblExplanation.Text = resources.GetString("lblExplanation.Text")
+        '
+        'lblCaution
+        '
+        Me.lblCaution.AutoSize = True
+        Me.lblCaution.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCaution.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCaution.ForeColor = System.Drawing.Color.Red
+        Me.lblCaution.Location = New System.Drawing.Point(12, 174)
+        Me.lblCaution.Name = "lblCaution"
+        Me.lblCaution.Size = New System.Drawing.Size(64, 15)
+        Me.lblCaution.TabIndex = 8
+        Me.lblCaution.Text = "CAUTION: "
+        '
+        'lblCautionText
+        '
+        Me.lblCautionText.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCautionText.Location = New System.Drawing.Point(81, 174)
+        Me.lblCautionText.Name = "lblCautionText"
+        Me.lblCautionText.Size = New System.Drawing.Size(318, 101)
+        Me.lblCautionText.TabIndex = 9
+        Me.lblCautionText.Text = resources.GetString("lblCautionText.Text")
+        '
+        'chkRecursiveScan
+        '
+        Me.chkRecursiveScan.AutoSize = True
+        Me.chkRecursiveScan.Checked = True
+        Me.chkRecursiveScan.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkRecursiveScan.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkRecursiveScan.Location = New System.Drawing.Point(287, 284)
+        Me.chkRecursiveScan.Name = "chkRecursiveScan"
+        Me.chkRecursiveScan.Size = New System.Drawing.Size(100, 17)
+        Me.chkRecursiveScan.TabIndex = 10
+        Me.chkRecursiveScan.Text = "Recursive scan"
+        Me.chkRecursiveScan.UseVisualStyleBackColor = True
+        '
+        'chkDarkMode
+        '
+        Me.chkDarkMode.AutoSize = True
+        Me.chkDarkMode.Checked = True
+        Me.chkDarkMode.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkDarkMode.Location = New System.Drawing.Point(12, 401)
+        Me.chkDarkMode.Name = "chkDarkMode"
+        Me.chkDarkMode.Size = New System.Drawing.Size(78, 17)
+        Me.chkDarkMode.TabIndex = 11
+        Me.chkDarkMode.Text = "Dark mode"
+        Me.chkDarkMode.UseVisualStyleBackColor = True
+        '
+        'lnkGithub
+        '
+        Me.lnkGithub.AutoSize = True
+        Me.lnkGithub.Location = New System.Drawing.Point(361, 402)
+        Me.lnkGithub.Name = "lnkGithub"
+        Me.lnkGithub.Size = New System.Drawing.Size(38, 13)
+        Me.lnkGithub.TabIndex = 12
+        Me.lnkGithub.TabStop = True
+        Me.lnkGithub.Text = "Github"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(411, 424)
+        Me.Controls.Add(Me.lnkGithub)
+        Me.Controls.Add(Me.chkDarkMode)
+        Me.Controls.Add(Me.chkRecursiveScan)
+        Me.Controls.Add(Me.lblCautionText)
+        Me.Controls.Add(Me.lblCaution)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.lblExplanation)
+        Me.Controls.Add(Me.lblPathText)
+        Me.Controls.Add(Me.btnRestore)
+        Me.Controls.Add(Me.btnAllow)
+        Me.Controls.Add(Me.btnBlock)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtPath)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Barricadr"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents dirPicker As FolderBrowserDialog
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents btnBlock As Button
+    Friend WithEvents btnAllow As Button
+    Friend WithEvents btnRestore As Button
+    Friend WithEvents lblPathText As Label
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents lblExplanation As Label
+    Friend WithEvents lblCaution As Label
+    Friend WithEvents lblCautionText As Label
+    Friend WithEvents chkRecursiveScan As CheckBox
+    Friend WithEvents chkDarkMode As CheckBox
+    Friend WithEvents lnkGithub As LinkLabel
+End Class
---
+++ Barricadr/frmMain.vb
@@ -0,0 +1,277 @@
+﻿Imports System.IO
+Imports System.Security.Principal
+Imports System.Text.RegularExpressions
+
+Public Class frmMain
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If Directory.Exists(txtPath.Text) Then
+            dirPicker.SelectedPath = txtPath.Text
+        End If
+
+        If dirPicker.ShowDialog = DialogResult.OK Then
+            txtPath.Text = dirPicker.SelectedPath
+        End If
+    End Sub
+
+    Private Sub ButtonHandler(sender As Object, e As EventArgs) Handles btnBlock.Click, btnAllow.Click, btnRestore.Click
+        Dim action As String = sender.Tag
+
+        If action = "restoreall" Then
+            If MessageBox.Show("This will permanently delete all rules set by Barricadr. Continue?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.Yes Then
+                lblHeader.Text = "Cleaning..."
+                'netsh advfirewall firewall show rule status=enabled name=all | FIND /I "Barricadr Block"
+                Dim tempBatchFile As String = System.IO.Path.GetTempPath() & Guid.NewGuid().ToString() & ".bat"
+                File.WriteAllText(tempBatchFile, $"@echo off{vbNewLine}netsh advfirewall firewall show rule status=enabled name=all | FIND /I ""Barricadr Block""")
+                Dim batchy As Process = New System.Diagnostics.Process()
+
+                With batchy.StartInfo
+                    .FileName = tempBatchFile
+                    .UseShellExecute = False
+                    .RedirectStandardOutput = True
+                    .CreateNoWindow = True
+                    .WindowStyle = ProcessWindowStyle.Hidden
+                End With
+
+                batchy.Start()
+                Dim batchyOutput As String = batchy.StandardOutput.ReadToEnd
+                batchy.WaitForExit()
+
+                File.Delete(tempBatchFile)
+
+                Dim rules As String() = batchyOutput.Split(vbNewLine)
+
+                For i As Integer = 0 To rules.Count - 1 Step 1
+                    rules(i) = Regex.Replace(rules(i).Trim(), "^Rule Name:\s+", "")
+                Next
+
+                RemoveAllRules(rules)
+
+                lblHeader.Text = "Barricadr"
+                If MessageBox.Show("Finished! Open Windows Firewall to review changes?", Application.ProductName,
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Information) = DialogResult.Yes Then
+                    Process.Start("wf.msc")
+                End If
+
+                btnRestore.Tag = "restore"
+                btnRestore.Text = "&Restore"
+                btnRestore.BackColor = Color.LightSkyBlue
+            End If
+        Else
+            Dim executables As List(Of String) = GetExecutables(txtPath.Text)
+
+            If executables.Count > 0 Then
+                Dim strList As String = ""
+                For Each entry In executables
+                    strList &= entry & vbNewLine
+                Next
+
+                Dim questionFragment As String = ""
+
+                If action = "block" Then
+                    questionFragment = "create firewall blocking rules"
+                ElseIf action = "allow" Then
+                    questionFragment = "create firewall allow rules"
+                ElseIf action = "restore" Then
+                    questionFragment = "remove all Barricadr-created firewall rules"
+                End If
+
+
+                Dim msg As String = $"Barricadr found {executables.Count} executable files in the chosen directory " &
+                                    $"tree. Are you sure you want to {questionFragment} for all of these " &
+                                    $"executables?{vbNewLine}{vbNewLine}Found executables:{vbNewLine}{strList}"
+
+                If MessageBox.Show(msg, Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.Yes Then
+                    lblHeader.Text = "Cleaning..."
+                    RemoveRules(executables)
+
+                    If Not action = "restore" Then
+                        For Each entry In executables
+                            If action = "block" Then
+                                lblHeader.Text = "Barricading..."
+                            Else
+                                lblHeader.Text = "Opening floodgates..."
+                            End If
+
+                            CreateRules(entry, action)
+                        Next
+                    End If
+
+                    lblHeader.Text = "Barricadr"
+                    If MessageBox.Show("Finished! Open Windows Firewall to review changes?", Application.ProductName,
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Information) = DialogResult.Yes Then
+                        Process.Start("wf.msc")
+                    End If
+                End If
+            Else
+                MessageBox.Show("No executables found within the given path, or the path is invalid.",
+                        Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        End If
+    End Sub
+
+    Private Function GetExecutables(dir) As List(Of String)
+        Dim recursive As SearchOption = SearchOption.AllDirectories
+        If chkRecursiveScan.Checked = False Then
+            recursive = SearchOption.TopDirectoryOnly
+        End If
+
+        If Directory.Exists(dir) Then
+            Return Directory.EnumerateFiles(dir, "*", recursive).Where(
+                Function(file) _
+                    file.ToLower().EndsWith(".exe") OrElse
+                    file.ToLower().EndsWith(".msi") OrElse
+                    file.ToLower().EndsWith(".msix") OrElse
+                    file.ToLower().EndsWith(".appx") OrElse
+                    file.ToLower().EndsWith(".appxbundle") OrElse
+                    file.ToLower().EndsWith(".bat") OrElse
+                    file.ToLower().EndsWith(".ps1") OrElse
+                    file.ToLower().EndsWith(".vbs") OrElse
+                    file.ToLower().EndsWith(".jar") OrElse
+                    file.ToLower().EndsWith(".cmd")
+            ).ToList()
+        Else
+            Return New List(Of String)
+        End If
+    End Function
+
+    Private Sub CreateRules(ByVal exePath As String, ByVal action As String)
+        Dim netshProcess As Process = New System.Diagnostics.Process()
+
+        With netshProcess.StartInfo
+            .FileName = "netsh"
+            .UseShellExecute = False
+            .RedirectStandardOutput = True
+            .CreateNoWindow = True
+            .WindowStyle = ProcessWindowStyle.Hidden
+        End With
+
+        netshProcess.StartInfo.Arguments = "advfirewall firewall add rule name=""Barricadr " & Char.ToUpper(action(0)) & action.Substring(1) & " Inbound: " & exePath & """ dir=in program=""" & exePath & """ action=" & action & " enable=yes"
+        netshProcess.Start()
+        netshProcess.WaitForExit()
+        netshProcess.StartInfo.Arguments = "advfirewall firewall add rule name=""Barricadr " & Char.ToUpper(action(0)) & action.Substring(1) & " Outbound: " & exePath & """ dir=out program=""" & exePath & """ action=" & action & " enable=yes"
+        netshProcess.Start()
+        netshProcess.WaitForExit()
+    End Sub
+
+    Private Sub RemoveRules(ByVal executables As List(Of String))
+        For Each entry In executables
+            Dim netshProcess As Process = New System.Diagnostics.Process()
+
+            With netshProcess.StartInfo
+                .FileName = "netsh"
+                .UseShellExecute = False
+                .RedirectStandardOutput = True
+                .CreateNoWindow = True
+                .WindowStyle = ProcessWindowStyle.Hidden
+            End With
+
+            netshProcess.StartInfo.Arguments = "advfirewall firewall delete rule name=""Barricadr Block Inbound: " & entry & """"
+            netshProcess.Start()
+            netshProcess.WaitForExit()
+            netshProcess.StartInfo.Arguments = "advfirewall firewall delete rule name=""Barricadr Allow Inbound: " & entry & """"
+            netshProcess.Start()
+            netshProcess.WaitForExit()
+            netshProcess.StartInfo.Arguments = "advfirewall firewall delete rule name=""Barricadr Block Outbound: " & entry & """"
+            netshProcess.Start()
+            netshProcess.WaitForExit()
+            netshProcess.StartInfo.Arguments = "advfirewall firewall delete rule name=""Barricadr Allow Outbound: " & entry & """"
+            netshProcess.Start()
+            netshProcess.WaitForExit()
+        Next
+    End Sub
+
+    Private Sub RemoveAllRules(ByVal rules As String())
+        For Each rule In rules
+            Dim netshProcess As Process = New System.Diagnostics.Process()
+
+            With netshProcess.StartInfo
+                .FileName = "netsh"
+                .UseShellExecute = False
+                .RedirectStandardOutput = True
+                .CreateNoWindow = True
+                .WindowStyle = ProcessWindowStyle.Hidden
+            End With
+
+            netshProcess.StartInfo.Arguments = "advfirewall firewall delete rule name=""" & rule & """"
+            netshProcess.Start()
+            netshProcess.WaitForExit()
+        Next
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Shown
+        'Check if we started as admin - if not, relaunch and quit
+        If Not WindowsIdentity.GetCurrent().Owner.IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid) Then
+            Dim p As Process = New System.Diagnostics.Process()
+
+            With p.StartInfo
+                .FileName = Application.ExecutablePath
+                .Verb = "RunAs"
+            End With
+
+            Try
+                p.Start()
+            Catch ex As Exception
+                MessageBox.Show("Barricadr requires administrator rights in order to function.", Application.ProductName,
+                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub frmMain_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.ShiftKey Then
+            btnRestore.Tag = "restoreall"
+            btnRestore.Text = "&Restore all"
+            btnRestore.BackColor = Color.Orange
+        End If
+    End Sub
+
+    Private Sub frmMain_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyCode = Keys.ShiftKey Then
+            btnRestore.Tag = "restore"
+            btnRestore.Text = "&Restore"
+            btnRestore.BackColor = Color.LightSkyBlue
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkGithub.LinkClicked
+        System.Diagnostics.Process.Start("https://github.com/xdpirate/Barricadr")
+    End Sub
+
+    Private Sub DarkMode(ByVal enable As Boolean)
+        If enable Then
+            Me.ForeColor = Color.White
+            Me.BackColor = Color.FromArgb(50, 50, 50)
+            txtPath.ForeColor = Color.White
+            txtPath.BackColor = Color.Black
+            btnBrowse.ForeColor = Color.White
+            btnBrowse.BackColor = Color.Black
+            btnBlock.ForeColor = Color.Black
+            btnAllow.ForeColor = Color.Black
+            btnRestore.ForeColor = Color.Black
+            lnkGithub.LinkColor = Color.White
+            lnkGithub.VisitedLinkColor = Color.Gray
+            lnkGithub.ActiveLinkColor = Color.DarkGray
+            lblCaution.ForeColor = Color.Salmon
+        Else
+            Me.ForeColor = SystemColors.ControlText
+            Me.BackColor = SystemColors.Control
+            txtPath.ForeColor = SystemColors.ControlText
+            txtPath.BackColor = SystemColors.Control
+            btnBrowse.ForeColor = SystemColors.ControlText
+            btnBrowse.BackColor = SystemColors.Control
+            btnBlock.ForeColor = SystemColors.ControlText
+            btnAllow.ForeColor = SystemColors.ControlText
+            btnRestore.ForeColor = SystemColors.ControlText
+            lnkGithub.LinkColor = Color.Blue
+            lnkGithub.VisitedLinkColor = Color.Purple
+            lnkGithub.ActiveLinkColor = Color.Red
+            lblCaution.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub chkDarkMode_CheckedChanged(sender As Object, e As EventArgs) Handles chkDarkMode.CheckedChanged
+        DarkMode(chkDarkMode.Checked)
+    End Sub
+End Class
